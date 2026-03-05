2db97d16367e9c11eda0ae3bd29cc45fe2654b8d
Mimi-elmo <elmoaouine.mariam@gmail.com>
2026-03-05 10:42:23 +0000

    First commit - ajout index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$teaPrice = 15;      
+$quantity = 5;       
+$isStudent = true;   
+
+$total = $teaPrice * $quantity;
+
+if ($quantity > 5) {
+    $total -= $quantity; 
+}
+
+if ($isStudent) {
+    $total -= $total * 0.20;  
+}
+
+echo "<h2>Moroccan Cafe</h2>";
+echo "<p>Thés : $quantity</p>";
+echo "<p>Prix par thé : $teaPrice DH</p>";
+echo "<p>Étudiant : " . ($isStudent ? "Oui" : "Non") . "</p>";
+echo "<hr>";
+echo "<h3>Total : $total DH</h3>";
+
+?>
