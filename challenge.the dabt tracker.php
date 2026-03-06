9c8ed95b62109053a46ac6ac2cea41c1fc9d75c2
Mimi-elmo <elmoaouine.mariam@gmail.com>
2026-03-06 12:29:20 +0000

    ajout challenge

---
+++ challenge.the dabt tracker.php
@@ -0,0 +1,25 @@
+<?php
+$friends=array(
+    "hamza" => 300,
+    "mariam" => 100,
+    "youssf" => 150,
+    "imane" => 90,
+    "sara" => 200
+    ) ;
+
+$total = 0;
+foreach($friends as $name =>$money) { 
+$total += $money;   
+if( $money >100) {  
+echo $name . " owes " . "$money" ." DH (High Debt)<br>";
+} else
+{
+echo $name ." owes " . "$money" ." DH<br>";
+}
+}
+echo "<br>Total money owed: " . $total . " DH";
+
+?>   
+
+
+
