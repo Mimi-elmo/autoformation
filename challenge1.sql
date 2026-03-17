54dfbcca7d273266b6b46729fb008603097a39eb
Mimi-elmo <elmoaouine.mariam@gmail.com>
2026-03-17 15:23:27 +0000

    Update challenge1.sql

--- challenge1.sql
+++ challenge1.sql
@@ -5,7 +5,7 @@ CREATE TABLE library_books (
     title VARCHAR(150) NOT NULL,
     author VARCHAR(100),
     published_year YEAR,
-    status ENUM('Available', 'Borrowed', 'Lost'),
+    status ENUM('Available', 'Borrowed', 'Lost')default('available'),
     price DECIMAL(10,2)
 );
 ?>
